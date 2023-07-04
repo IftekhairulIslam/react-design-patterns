aac59068d017a94b744400dc2265e8dcd3415d8e
Iftekhairul Islam <iftekhairul@alphacommerz.com>
2023-07-04 09:05:08 +0600

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React Design Patterns</h1>
+      <div className="card">
+        <p>
+          Branches wise patterns were given and branch commits wise pattern
+          evaluations were shown.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
